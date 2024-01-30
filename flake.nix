f851fe6e1deaf6b481790b2496458ece0f42e966
siph <dawkins.chris.dev@gmail.com>
2024-01-29 21:16:20 -0700

    refactor nupm-test

--- flake.nix
+++ flake.nix
@@ -3,18 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # `0.88.1`
-    nixpkgs-nushell.url = "github:NixOS/nixpkgs/baa02207279115f5457c18fef3fc3513f58fa8d2";
+    # `0.89.0`
+    nixpkgs-nushell.url = "github:NixOS/nixpkgs/ad22350c46fb528e3bd27fb08943c9d500852f02";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nupm = {
-      url = "github:siph/nupm/flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    nupm = {
+      url = "github:nushell/nupm";
+      flake = false;
+    };
   };
 
   outputs = inputs @ {
@@ -45,86 +45,98 @@
         self',
         system,
         ...
-      }: let
-        inherit (nixpkgs-nushell.legacyPackages.${system}) nushell;
-        treefmtWrapper = config.treefmt.build.wrapper;
-        buildInputs = with pkgs; [nushell yt-dlp];
-        nupm-test = nupm.packages.${system}.nupm-test;
-      in {
-        packages = with pkgs; rec {
-          yt-watcher = stdenvNoCC.mkDerivation rec {
-            nativeBuildInputs = [makeBinaryWrapper];
-            inherit buildInputs;
-            name = "yt-watcher";
-            src = ./.;
+      }:
+        with pkgs; let
+          inherit (nixpkgs-nushell.legacyPackages.${system}) nushell;
+          treefmtWrapper = config.treefmt.build.wrapper;
+          buildInputs = [nushell yt-dlp];
+          nupm-test = writeShellApplication {
+            runtimeInputs = [nupm nushell];
+            name = "nupm-test";
+            text = ''
+              nu --no-config-file \
+                --commands '
+                  use ${nupm}/nupm
 
-            # I don't know if there is any meaningful difference between
-            # ```shell
-            # nu \
-            # --no-config-file \
-            # --commands 'use ./yt-watcher ; yt-watcher'
-            # ```
-            #
-            # and
-            #
-            # ```shell
-            # nu ./yt-watcher/mod.nu
-            # ```
-            installPhase = ''
-              mkdir -p $out/bin
-              mkdir -p $out/share/${name}
-              mv ./* $out/share/${name}
-              makeWrapper ${nushell}/bin/nu $out/bin/${name} \
-                --add-flags $out/share/${name}/${name}/mod.nu
+                  nupm test
+                '
             '';
           };
-          default = yt-watcher;
-        };
-
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks.treefmt.enable = true;
-            settings.treefmt.package = treefmtWrapper;
-          };
-
-          nushell-tests = with pkgs;
-            stdenv.mkDerivation {
-              inherit system;
-              name = "nushell tests";
+        in {
+          packages = with pkgs; rec {
+            yt-watcher = stdenvNoCC.mkDerivation rec {
+              nativeBuildInputs = [makeBinaryWrapper];
+              inherit buildInputs;
+              name = "yt-watcher";
               src = ./.;
-              nativeBuildInputs = [nushell nupm-test];
-              buildInputs = [wiremock];
-              buildPhase = ''
-                ${wiremock}/bin/wiremock \
-                  --disable-banner \
-                  --verbose \
-                  --root-dir ./tests &
 
-                sleep 2
-
-                nupm-test
-              '';
+              # I don't know if there is any meaningful difference between
+              # ```shell
+              # nu \
+              # --no-config-file \
+              # --commands 'use ./yt-watcher ; yt-watcher'
+              # ```
+              #
+              # and
+              #
+              # ```shell
+              # nu ./yt-watcher/mod.nu
+              # ```
               installPhase = ''
-                touch $out
+                mkdir -p $out/bin
+                mkdir -p $out/share/${name}
+                mv ./* $out/share/${name}
+                makeWrapper ${nushell}/bin/nu $out/bin/${name} \
+                  --add-flags $out/share/${name}/${name}/mod.nu
               '';
             };
-        };
+            default = yt-watcher;
+          };
 
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs = {
-            alejandra.enable = true;
-            # nufmt.enable = true;
+          checks = {
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks.treefmt.enable = true;
+              settings.treefmt.package = treefmtWrapper;
+            };
+
+            nushell-tests = with pkgs;
+              stdenv.mkDerivation {
+                inherit system;
+                name = "nushell tests";
+                src = ./.;
+                nativeBuildInputs = [nushell nupm-test];
+                buildInputs = [wiremock];
+                buildPhase = ''
+                  ${wiremock}/bin/wiremock \
+                    --disable-banner \
+                    --verbose \
+                    --root-dir ./tests &
+
+                  sleep 2
+
+                  nupm-test
+                '';
+                installPhase = ''
+                  touch $out
+                '';
+              };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              alejandra.enable = true;
+              # nufmt.enable = true;
+            };
+          };
+
+          devShells = with pkgs; {
+            default = mkShell {
+              inherit (self'.checks.pre-commit-check) shellHook;
+              buildInputs = buildInputs ++ [nupm-test treefmtWrapper wiremock];
+            };
           };
         };
-
-        devShells = with pkgs; {
-          default = mkShell {
-            inherit (self'.checks.pre-commit-check) shellHook;
-            buildInputs = buildInputs ++ [treefmtWrapper wiremock];
-          };
-        };
-      };
     };
 }
---
+++ tests/mod.nu
@@ -0,0 +1,4 @@
+export use ./config.nu
+export use ./invidious.nu
+export use ./utils.nu
+export use ./youtube.nu
